e299b17d74ad362026204f9453ae79b43824be3f
DavidOsornio98 <61331563+DavidOsornio98@users.noreply.github.com>
2020-02-25 13:59:29 -0600

    Add files via upload

---
+++ Examen/cambio.js
@@ -0,0 +1,69 @@
+function oscuro() {
+    document.getElementById("parrafo").id = "parrafoD";
+    document.getElementById("cabeza").id ="cabezaD";
+    document.getElementById("imagen1").id ="imagen1D";
+    document.getElementById("imagen2").id ="imagen2D";
+    document.getElementById("art1").id ="art1D";
+    document.getElementById("art2").id ="art2D";
+    document.getElementById("art3").id ="art3D";
+    document.getElementById("art4").id ="art4D";
+    document.getElementById("cuerpo").id ="cuerpoD";
+    document.getElementById("op0").id = "op0Dark";
+    document.getElementById("op1").id = "op1Dark";
+    document.getElementById("op2").id = "op2Dark";
+    document.getElementById("op3").id = "op3Dark";
+    document.getElementById("op4").id = "op4Dark";
+    document.getElementById("op5").id = "op5Dark";
+    document.getElementById("op6").id = "op6Dark";
+    document.getElementById("op7").id = "op7Dark";
+    document.getElementById("op8").id = "op8Dark";
+    document.getElementById("op9").id = "op9Dark";
+    document.getElementById("op10").id = "op10Dark";
+    document.getElementById("op11").id = "op11Dark";
+    document.getElementById("op12").id = "op12Dark";
+    document.getElementById("op13").id = "op3Dark";
+    document.getElementById("op14").id = "op14Dark";
+    document.getElementById("op15").id = "op15Dark";
+    document.getElementById("op16").id = "op16Dark";
+    document.getElementById("op17").id = "op17Dark";
+    document.getElementById("op18").id = "op18Dark";
+    document.getElementById("op19").id = "op19Dark";
+    document.getElementById("pie").id = "pieD";
+    document.getElementById("op19").id = "op19Dark";
+
+
+}
+
+function normal() {
+    document.getElementById("parrafoD").id = "parrafo";
+    document.getElementById("cabezaD").id ="cabeza";
+    document.getElementById("imagen1D").id ="imagen1";
+    document.getElementById("imagen2D").id ="imagen2";
+    document.getElementById("art1D").id ="art1";
+    document.getElementById("art2D").id ="art2";
+    document.getElementById("art3D").id ="art3";
+    document.getElementById("art4D").id ="art4";
+    document.getElementById("cuerpoD").id ="cuerpo";
+    document.getElementById("op0Dark").id = "op0";
+    document.getElementById("op1Dark").id = "op1";
+    document.getElementById("op2Dark").id = "op2";
+    document.getElementById("op3Dark").id = "op3";
+    document.getElementById("op4Dark").id = "op4";
+    document.getElementById("op5Dark").id = "op5";
+    document.getElementById("op6Dark").id = "op6";
+    document.getElementById("op7Dark").id = "op7";
+    document.getElementById("op8Dark").id = "op8";
+    document.getElementById("op9Dark").id = "op9";
+    document.getElementById("op10Dark").id = "op10";
+    document.getElementById("op11Dark").id = "op11";
+    document.getElementById("op12Dark").id = "op12";
+    document.getElementById("op13Dark").id = "op3";
+    document.getElementById("op14Dark").id = "op14";
+    document.getElementById("op15Dark").id = "op15";
+    document.getElementById("op16Dark").id = "op16";
+    document.getElementById("op17Dark").id = "op17";
+    document.getElementById("op18Dark").id = "op18";
+    document.getElementById("op19Dark").id = "op19";
+    document.getElementById("pieD").id = "pie";
+
+}
